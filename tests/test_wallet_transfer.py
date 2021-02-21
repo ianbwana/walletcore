83beace3550b9e597e509352e8080fb983294f4c
ian bwana <ib@sky.garden>
2021-02-22 01:33:33 +0300

    changed coverage run command

--- tests/test_wallet_transfer.py
+++ tests/test_wallet_transfer.py
@@ -1,8 +1,2 @@
 from rest_framework.test import APIRequestFactory
 from django.test.client import encode_multipart, RequestFactory
-
-factory = RequestFactory()
-data = {'title': 'remember to email dave'}
-content = encode_multipart('BoUnDaRyStRiNg', data)
-content_type = 'multipart/form-data; boundary=BoUnDaRyStRiNg'
-request = factory.put('/notes/547/', content, content_type=content_type)
